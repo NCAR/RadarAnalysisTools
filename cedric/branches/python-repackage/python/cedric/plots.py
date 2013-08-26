6791af5e98192520bf7e408055765377161905d5
Gary Granger <granger@ucar.edu>
2013-08-26 21:27:03 +0000

    allow plot levels to be specified, and default to something which works a little better for both reflectivity and velocity but which is still arbitrary
    git-svn-id: https://proxy.subversion.ucar.edu/RadAnl_Tools@176 569d80e6-d933-c0f4-fcc6-90685d5d544e

--- cedric/branches/python-repackage/python/cedric/plots.py
+++ cedric/branches/python-repackage/python/cedric/plots.py
@@ -11,25 +11,24 @@ def inspect_field(field):
     plt.close()
 
 
-def contourf(field):
+def contourf(field, levels):
     # The height of the slice comes from the third index, assuming this
     # was sliced horizontally...
     iheight = field.index()[2]
     grid = field.volume().grid
     height = grid.z.first + iheight*grid.z.delta
-    levels=xrange(-5,55,10)
     cs = plt.contourf(field.data(), levels)
     cbar = plt.colorbar(cs)
     plt.title("%s / %s @ %f %s" % (field.volume().title, field.name(),
                                    height, "km"))
 
-def compare_fields(f1, f2):
+def compare_fields(f1, f2, levels=xrange(-60,60,5)):
     "Create a figure with side-by-side plots of the 2-D variables."
     fig = plt.figure(figsize=(20,10))
     gs = gridspec.GridSpec(1, 2)
     axleft = plt.subplot(gs[0])
-    contourf(f1)
+    contourf(f1, levels)
     axright = plt.subplot(gs[1])
-    contourf(f2)
+    contourf(f2, levels)
     plt.show()
     plt.close()
